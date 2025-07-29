1d54c160581e445118bcd93130ef68de6fe94446
Ubuntu <ubuntu@ip-172-31-47-225.ec2.internal>
2025-07-29 10:49:14 +0000

    Changed templates/serving code to work with a reverse proxy and multiple servers on a domain.

--- main.go
+++ main.go
@@ -1,27 +1,52 @@
 package main
 
 import (
-    "fmt"
     "net/http"
     "html/template"
+    "os"
+    "log"
 )
 
 
+type TemplateData struct {
+    BasePath string
+}
 
 
 func main() {
+    if len(os.Args) != 2 {
+        log.Println(`
+            USAGE:
+                go run main.go PORT
+        `)
+        os.Exit(1)
+    }
+    port := os.Args[1]
     
     fs := http.FileServer(http.Dir("./static"))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
     tmpl := template.Must(template.ParseFiles("templates/index.html"))
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        tmpl.Execute(w, nil)
+        basePath := r.Header.Get("SCRIPT_NAME")
+
+        if basePath == "/" {
+            basePath = ""
+        }
+
+        data := TemplateData{
+            BasePath: basePath,
+        }
+
+        if err := tmpl.Execute(w, data); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
     })
 
 
-    fmt.Println("Listening on port 8086...")
-    http.ListenAndServe(":8086", nil)
-    
+    log.Println("Listening on port " + port)
+    if err := http.ListenAndServe(":" + port, nil); err != nil {
+        log.Fatal(err)
+    }
     
 }
